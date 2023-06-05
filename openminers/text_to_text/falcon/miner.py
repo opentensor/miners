a14c91f08f264b79ed1dbdbbc660b0fc377826f6
ifrit98 <ifrit98@gmail.com>
2023-06-05 15:47:14 +0000

    fix falcon miner device

--- openminers/text_to_text/falcon/miner.py
+++ openminers/text_to_text/falcon/miner.py
@@ -15,7 +15,6 @@
 # OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 # DEALINGS IN THE SOFTWARE.
 
-import os; os.environ['CUDA_VISIBLE_DEVICES'] = '1,2,3,4,5'
 import time
 import torch
 import argparse
@@ -41,22 +40,27 @@ class FalconMiner( openminers.BaseMiner ):
     @classmethod
     def add_args( cls, parser: argparse.ArgumentParser ):
         parser.add_argument( '--falcon.model_name', type=str, default="tiiuae/falcon-7b-instruct", help='Name/path of model to load' )
-        parser.add_argument( '--falcon.device', type=str, help='Device to load model', default="cuda" )
+        parser.add_argument( '--falcon.device', type=int, help='Device to load model (integer GPU slot)', default=0 )
         parser.add_argument( '--falcon.device_map', type=str, help='Device map for model', default=None )
         parser.add_argument( '--falcon.max_length', type=int, help='Max tokens for model output.', default=100 )
         parser.add_argument( '--falcon.temperature', type=float, help='Sampling temperature of model', default=0.5 )
-        parser.add_argument( '--falcon.top_k', type=int, help='Top k sampling of model', default=10 )
+        parser.add_argument( '--falcon.top_k', type=int, help='Top k sampling of model', default=1 )
         parser.add_argument( '--falcon.do_sample', action='store_true', default=False, help='Whether to use sampling or not (if not, uses greedy decoding).' )
         parser.add_argument( '--falcon.do_prompt_injection', action='store_true', default=False, help='Whether to use a custom "system" prompt instead of the one sent by bittensor.' )
         parser.add_argument( '--falcon.system_prompt', type=str, help='What prompt to replace the system prompt with', default= "A chat between a curious user and an artificial intelligence assistant.\nThe assistant gives helpful, detailed, and polite answers to the user's questions. " )
         parser.add_argument( '--falcon.num_return_sequences', type=int, help='Number of sequences to return', default=1 )
         parser.add_argument( '--falcon.repetition_penalty', type=float, help='Repetition penalty for model', default=1.9 )
 
+    @classmethod
+    def config( cls ) -> "bittensor.Config":
+        parser = argparse.ArgumentParser( description='Falcon Miner Config' )
+        cls.add_args( parser )
+        return bittensor.config( parser )
+
     def __init__( self, *args, **kwargs):
         super( FalconMiner, self ).__init__( *args, **kwargs )
 
         bittensor.logging.info( 'Loading ' + str( self.config.falcon.model_name ) )
-
         self.tokenizer = AutoTokenizer.from_pretrained( self.config.falcon.model_name )
         self.stop_token_ids = self.tokenizer.convert_tokens_to_ids( ["</s>","<|endoftext|>"] )
         self.stop = StopOnTokens( self.stop_token_ids )
@@ -68,6 +72,7 @@ class FalconMiner( openminers.BaseMiner ):
             trust_remote_code=True,
             temperature=self.config.falcon.temperature,
             do_sample=self.config.falcon.do_sample,
+            device_map=self.config.falcon.device_map
         )
 
         if self.config.falcon.device_map is not None:
@@ -75,8 +80,7 @@ class FalconMiner( openminers.BaseMiner ):
         else:
             kwargs['device'] = self.config.falcon.device
 
-        self.pipe = pipeline( "text-generation",  **kwargs )
-
+        self.model = pipeline( "text-generation",  **kwargs )
         bittensor.logging.info( 'Model loaded!' )
 
         if self.config.falcon.device != "cpu" and self.config.falcon.device_map is not None:
@@ -99,17 +103,17 @@ class FalconMiner( openminers.BaseMiner ):
     def forward( self, messages: List[Dict[str, str]] ) -> str:
         history = self._process_history( messages )
         prompt = history + "ASSISTANT:"
- 
-        generation = self.pipe(
+
+        generation = self.model(
             prompt,
             max_length=self.config.falcon.max_length,
             do_sample=self.config.falcon.do_sample,
             top_k=self.config.falcon.top_k,
             num_return_sequences=self.config.falcon.num_return_sequences,
-            eos_token_id=self.pipe.tokenizer.eos_token_id,
-            pad_token_id=self.pipe.tokenizer.pad_token_id,
+            eos_token_id=self.tokenizer.eos_token_id,
+            pad_token_id=self.tokenizer.pad_token_id,
             repetition_penalty=self.config.falcon.repetition_penalty,
-            stopping_criteria=StoppingCriteriaList( [self.stop] )
+            stopping_criteria=StoppingCriteriaList( [self.stop] ),
         )
 
         # Logging input and generation if debugging is active
@@ -117,8 +121,11 @@ class FalconMiner( openminers.BaseMiner ):
         bittensor.logging.debug( "Generation: " + str( generation ) )
         return generation
 
-if __name__ == "__main__":  
-    with FalconMiner():
-        while True:
-            print ('running...', time.time() )
-            time.sleep( 1 )
+if __name__ == "__main__":
+    miner = FalconMiner()
+    messages = [{'content': 'Hello', 'role': 'user'}, {'content': 'You are a helpful assistant who is often sarcastic.', 'role': 'system'}]
+    print(miner.forward(messages))
+    # with FalconMiner():
+    #     while True:
+    #         print ('running...', time.time() )
+    #         time.sleep( 1 )
