23c01c1c56297c504c5778b640bd7e1ce28e79b3
ifrit98 <ifrit98@gmail.com>
2023-06-14 21:46:09 +0000

    add set_weights in separate file

---
+++ openminers/base/set_weights.py
@@ -0,0 +1,23 @@
+import torch
+import wandb
+import bittensor as bt
+
+def set_weights( subtensor: "bt.subtensor", netuid: int, uid: int, wallet: "bt.wallet" ) -> None:
+    try:
+        # --- query the chain for the most current number of peers on the network
+        chain_weights = torch.zeros( subtensor.subnetwork_n( netuid = netuid ))
+        chain_weights[uid] = 1
+        subtensor.set_weights(
+            uids=torch.arange(0, len(chain_weights)),
+            netuid=netuid,
+            weights=chain_weights,
+            wait_for_inclusion=False,
+            wallet=wallet,
+            version_key=1
+        )
+        wandb.log( {'set_weights': 1 } )
+
+    except Exception as e:
+        # --- Set weights.
+        wandb.log( {'set_weights': 0} )
+        bt.logging.error( f"Failed to set weights on chain with exception: { e }" )
