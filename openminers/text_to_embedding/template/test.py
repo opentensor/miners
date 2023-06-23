d623a046857b5153a6230ecf28f2b79c29242195
ifrit98 <ifrit98@gmail.com>
2023-06-23 03:42:30 +0000

    removes /miners subdir

--- openminers/text_to_embedding/template/test.py
+++ openminers/text_to_embedding/template/test.py
@@ -4,18 +4,24 @@ import requests
 import bittensor as bt
 bt.trace()
 
-hotkey = '5C86aJ2uQawR6P6veaJQXNK9HaWh6NMbUhTiLs65kq4ZW3NH'
+# This hotkey must be registered unless you set --blacklist.allow_non_registered = true in the config or return False in the Synapse().blacklist() function.
+hotkey = '<Your hotkey here>'
 text = "(beautiful) (best quality) mdjrny-v4 style Pepe the frog enormous, surrounded by colorful sea creatures and plants, - surreal, Dreamlike, ethereal lighting, Highly detailed, Intricate, Digital painting, Artstation, Concept art, Smooth, Sharp focus, Fantasy, trending on art websites, art by magali villeneuve and jock and ashley wood and rachel lee and loish"
 texts = ["This is a list of strings", "A second string"]
 
+# Package the request to be parsed by a Pydantic BaseModel on the server.
 data = {
   "text": text,
   "timeout": 12,
 }
+
+# Send the request to the server. 
 req = requests.post('http://127.0.0.1:8092/TextToEmbedding/Forward/?hotkey={}'.format(hotkey), json=data)
 print(req)
 print(req.text)
 
+# Parse the response embedding.
+# TODO: this should be a base64 encoded string, not List[List[float]]
 x = json.loads(req.text)
 emb = torch.Tensor(x)
 print("emb.shape: ", emb.shape)
